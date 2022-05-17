f05e8be8b5e86ebc1d213dad266ff2e4d1fc5c40
Ashenweerasinghe <85500834+Ashenweerasinghe@users.noreply.github.com>
2022-05-17 20:28:47 +0530

    Updated

--- Application 2/Testing7(Gen).py
+++ Application 2/Testing7(Gen).py
@@ -1,28 +1,8 @@
-import sqlite3
-import tkinter.messagebox as mb
 from tkinter import *
-import sqlite3
-import tkinter.messagebox as mb
-from tkinter import filedialog, ttk
-from tkinter.filedialog import askopenfilename
-import customtkinter
-import tk as tk
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolbar2Tk)
-from tkinter.ttk import Progressbar
-import time
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from tkinter.filedialog import askopenfilename
 import customtkinter
 import pandas as pd
-from tkinter import ttk
-import tkinter as tk
-import csv
-from keras.layers import LSTM, Dense
-from keras.models import Sequential
-from numpy import array
-import time
+
 
 def get_data_frame11():
     global df_a11
@@ -36,16 +16,16 @@ def Graphs():
     #root.destroy()
     global root2
     root2 = Tk()
-    root2.geometry("540x708")
+    root2.geometry("480x708")
     root2.resizable(False, False)
     root2["bg"] = "#161C30"
     root2.title("Genetrix")
 
     Label(root2, text="Welcome To Genetrix Analysis", bg="black", fg="white", font=("monospace", 20, "bold"), width=40, bd=4,relief=RIDGE).pack(side=TOP, fill=X)
-    customtkinter.CTkButton(root2, text="BROWSE", bd=0, height=50, width=510, text_color="#161C30",fg_color="#ffffff", text_font=('arial', 22),command=get_data_frame11).place(x=18, y=240)
-    customtkinter.CTkButton(root2, text="ANALYSE", bd=0, height=50, width=510, text_color="#161C30",fg_color="#ffffff", text_font=('arial', 22)).place(x=18, y=310)
-    customtkinter.CTkButton(root2, text="GENERATE", bd=0, height=50, width=510, text_color="#161C30", fg_color="#ffffff",text_font=('arial', 22)).place(x=18, y=380)
-    customtkinter.CTkButton(root2, text="BACK", bd=0, height=50, width=514, text_color="#161C30", fg_color="#ffffff",text_font=('arial', 22), command="MainBack").place(x=18, y=450)
+    customtkinter.CTkButton(root2, text="BROWSE", bd=0, height=50, width=250, text_color="#161C30",fg_color="#ffffff", text_font=('arial', 22),command=get_data_frame11).place(x=110, y=240)
+    customtkinter.CTkButton(root2, text="ANALYSE", bd=0, height=50, width=250, text_color="#161C30",fg_color="#ffffff", text_font=('arial', 22)).place(x=110, y=310)
+    customtkinter.CTkButton(root2, text="GENERATE", bd=0, height=50, width=250, text_color="#161C30", fg_color="#ffffff",text_font=('arial', 22)).place(x=110, y=380)
+    customtkinter.CTkButton(root2, text="BACK", bd=0, height=50, width=250, text_color="#161C30", fg_color="#ffffff",text_font=('arial', 22), command="MainBack").place(x=110, y=450)
 
     root2.mainloop()
 
